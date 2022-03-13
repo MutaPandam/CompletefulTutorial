b8e08d9bf16e522d51a970955374392b82d088d1
MutaPandam <47093943+MutaPandam@users.noreply.github.com>
2022-03-13 14:50:01 -0500

    Hello World
    Created a Hello World controller

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //My Hello World
+
+        // default Hello World action
+        // called when no index is provided
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is the Index/default action.";
+        }
+
+        // Welcome action
+        // Is passed name and numTimes via the URL
+        // Ex: https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
