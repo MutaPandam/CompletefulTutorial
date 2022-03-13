646a9477d7df777db89b56ad711ee66715a562ac
MutaPandam <47093943+MutaPandam@users.noreply.github.com>
2022-03-13 16:14:00 -0500

    Create Movie.cs
    Added a movies class

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    // class used to create movie objects that will be stored in the database and displayed on the movies page.
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
